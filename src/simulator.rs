13d43ab6ea3b20d7b5612114dd008da7cb104d83
Souvlaki42 <ilias@moulas.dev>
2026-01-14 15:59:53 +0200

    fix: rework to token, take stuff from assembler and actually execute the code

--- src/simulator.rs
+++ src/simulator.rs
@@ -1,44 +1,99 @@
+use std::collections::HashMap;
+
+use thiserror::Error;
+
 use crate::{
-    assembler::Assembler,
-    cpu::{Cpu, CpuError, Register},
+    assembler::{BASE_DATA_ADDR, Instruction, MEMORY_SIZE},
+    registers::{Register, RegisterError, RegisterFile},
 };
+
+#[derive(Debug, Error)]
+pub enum SimulatorError {
+    #[error("Register error: {0}")]
+    RegisterError(#[from] RegisterError),
+    #[error("No more instructions")]
+    NoMoreInstructions,
+}
 
 #[derive(Debug)]
 pub struct Simulator {
-    memory: Vec<u8>,
-    cpu: Cpu,
-    assembler: Assembler,
+    memory: [u8; MEMORY_SIZE],
+    registers: RegisterFile,
+    instructions: HashMap<u32, Instruction>,
+    pc: u32,
 }
 
-const MEMORY_SIZE: usize = 1024 * 1024;
-
 impl Simulator {
-    pub fn new() -> Simulator {
+    pub fn new(instructions: HashMap<u32, Instruction>, memory: Vec<u8>, entry: u32) -> Simulator {
+        let mut mem_array = [0u8; MEMORY_SIZE];
+        let len = memory.len().min(MEMORY_SIZE);
+        mem_array[..len].copy_from_slice(&memory[..len]);
+
         Simulator {
-            memory: vec![0; MEMORY_SIZE],
-            cpu: Cpu::new(),
-            assembler: Assembler::new(),
+            memory: mem_array,
+            registers: RegisterFile::default(),
+            instructions,
+            pc: entry,
         }
     }
 
-    fn get_register(&self, register: &str) -> Result<u32, CpuError> {
-        let reg = register.parse::<Register>()?;
-        Ok(self.cpu.gprs.get(reg))
-    }
-
-    fn set_register(&mut self, register: &str, value: u32) -> Result<(), CpuError> {
-        let reg = register.parse::<Register>()?;
-        self.cpu.gprs.set(reg, value);
+    fn execute_instruction(&mut self, instruction: Instruction) -> Result<(), SimulatorError> {
+        match instruction {
+            Instruction::AddImmediate { res, reg, imm } => {
+                let value = self.registers.get(reg).wrapping_add(imm as u32);
+                self.registers.set(res, value);
+            }
+            Instruction::LoadUpperImmediate { res, imm } => {
+                let value = (imm as u32) << 16;
+                self.registers.set(res, value);
+            }
+            Instruction::OrImmediate { res, reg, imm } => {
+                let value = self.registers.get(reg) | (imm as u32);
+                self.registers.set(res, value);
+            }
+            Instruction::SystemCall => {
+                self.handle_syscall()?;
+            }
+        }
         Ok(())
     }
 
-    pub fn run(&mut self, file: &str) -> Result<(), Box<dyn std::error::Error>> {
-        let tokens = tokenize(file)?;
+    fn handle_syscall(&mut self) -> Result<(), SimulatorError> {
+        let v0 = self.registers.get(Register::V0);
+        match v0 {
+            4 => {
+                let addr = self.registers.get(Register::A0) as usize;
+                let offset = addr - BASE_DATA_ADDR as usize;
 
-        self.assembler.assemble(tokens)?;
+                let mut bytes = Vec::new();
+                let mut i = offset;
+                while i < self.memory.len() && self.memory[i] != 0 {
+                    bytes.push(self.memory[i]);
+                    i += 1;
+                }
 
-        println!("{:?}", self.assembler);
+                let s = String::from_utf8_lossy(&bytes);
+                print!("{}", s);
+            }
+            10 => {
+                return Err(SimulatorError::NoMoreInstructions);
+            }
+            _ => {
+                println!("Unknown syscall: {}", v0);
+            }
+        }
+        Ok(())
+    }
 
+    pub fn step(&mut self) -> Result<(), SimulatorError> {
+        let instruction = self
+            .instructions
+            .get(&self.pc)
+            .ok_or(SimulatorError::NoMoreInstructions)?
+            .clone();
+
+        self.execute_instruction(instruction)?;
+        self.pc += 4;
         Ok(())
     }
 }
